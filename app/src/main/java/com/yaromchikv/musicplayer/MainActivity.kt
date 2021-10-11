9a7ee2279cead48aa7896f156d639980ef33b17f
Vladislav Yaromchik <vladislav.yaromchik@gmail.com>
2021-10-11 13:24:12 +0300

    feat: add PlayerFragment

--- app/src/main/java/com/yaromchikv/musicplayer/MainActivity.kt
+++ app/src/main/java/com/yaromchikv/musicplayer/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.yaromchikv.musicplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, PlayerFragment.newInstance())
+            .commit()
     }
 }
